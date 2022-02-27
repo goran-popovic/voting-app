2042aa8a4ca42298e42d724c6f5d34df8d9c885c
Goran Popovic <geoligard@gmail.com>
2022-02-28 00:33:09 +0100

    Update read me, and minor style updates

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -5,8 +5,8 @@ import { Link } from '@inertiajs/inertia-vue3';
 const props = defineProps(['href', 'active']);
 
 const classes = computed(() => props.active
-    ? 'inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-yellow-400 transition duration-150 ease-in-out'
-    : 'inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-yellow-400 transition duration-150 ease-in-out'
+    ? 'inline-flex items-center pt-1 text-sm font-medium leading-5 text-yellow-400 transition duration-150 ease-in-out'
+    : 'inline-flex items-center pt-1 text-sm font-medium leading-5 text-yellow-400 transition duration-150 ease-in-out'
 );
 </script>
 
